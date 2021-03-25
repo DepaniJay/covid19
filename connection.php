206e78a298fb1a71ae48d574faca6c1ed5b7a3ef
DepaniJay <jrdepani@gmail.com>
2021-03-25 18:14:43 +0530

    add remote database connection

--- connection.php
+++ connection.php
@@ -1,11 +1,23 @@
 <?php
 
-$server = "localhost";
-$username = "root";
-$password = "";
-$db = "test";
+// Remote server Connection
 
-$con = mysqli_connect($server,$username,$password,$db);
+$server = "remotemysql.com";
+$username = "ROLazsorpS";
+$password = "KUYSrzJXnK";
+$db = "ROLazsorpS";
+$port = 3306;
+
+$con = mysqli_connect($server,$username,$password,$db,$port);
+
+// Localserver Connections
+
+// $server = "localhost";
+// $username = "root";
+// $password = "";
+// $db = "test";
+
+// $con = mysqli_connect($server,$username,$password,$db);
 
 if($con){
     ?>
